5e32cbba2e642668d9347491ea9dde28a06c2ef8
Goran Jovic <goranjovic@gmail.com>
2016-12-06 22:00:33 +0100

    isolated actual maven embedder in a pod to allow multiple versions

--- build.boot
+++ build.boot
@@ -1,9 +1,8 @@
 (def project 'big-solutions/boot-mvn)
-(def version "0.1.1")
+(def version "0.1.2-SNAPSHOT")
 
 (set-env! :source-paths #{"src"}
           :dependencies   '[[org.clojure/clojure "1.7.0"]
-                            [org.apache.maven/maven-embedder "3.1.1"]
 
                             [boot/core "2.6.0" :scope "test"]
                             [onetom/boot-lein-generate "0.1.3" :scope "test"]
--- src/boot_mvn/core.clj
+++ src/boot_mvn/core.clj
@@ -1,13 +1,19 @@
 (ns boot-mvn.core
   {:boot/export-tasks true}
-  (:require [boot.core :as boot :refer [deftask]])
-  (:import (org.apache.maven.cli MavenCli)))
+  (:require [boot.core :as boot :refer [deftask]]
+            [boot.pod :as pod]))
 
 (deftask mvn
   "Run Maven commands from Boot"
-  [A args ARGS str "Maven commands and options"]
-  (let [mvn (new MavenCli)
-        params (.split ^String args "\\s+")]
-    (println (vec params))
-    (.doMain mvn params "." System/out System/err))
+  [A args ARGS str "Maven commands and options"
+   V version VERSION str "Maven version"]
+  (let [maven-version (or version "3.1.1")
+        pod (pod/make-pod (update-in (boot/get-env)
+                                     [:dependencies]
+                                     conj `[org.apache.maven/maven-embedder ~maven-version]))]
+    (pod/with-eval-in pod
+                      (require '[boot.core :as boot])
+                      (import org.apache.maven.cli.MavenCli)
+                      (.doMain (new org.apache.maven.cli.MavenCli)
+                               (.split ^String ~args "\\s+")"." System/out System/err)))
   identity)
